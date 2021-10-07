dcced1bce9c2c051effdc923eb05de6a0e01a925
jinyangb <your_email@example.com>
2021-10-07 13:44:09 -0400

    adding submit fields

--- client/src/App.vue
+++ client/src/App.vue
@@ -27,6 +27,6 @@
 }
 
 #nav a.router-link-exact-active {
-  color: #42b983;
+  color: #03cae4;
 }
 </style>
---
+++ client/src/pages/About.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <p3>Not sure if we want an about page</p3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'About'
+}
+</script>
